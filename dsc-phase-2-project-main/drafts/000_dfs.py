88aaa5b331d4663d4e652f50b258f4a953e3011f
Claire Sarraillé <clairesarraille@outlook.com>
2021-10-04 22:11:37 -0700

    updated workflow

---
+++ dsc-phase-2-project-main/drafts/000_dfs.py
@@ -0,0 +1,3 @@
+R-Squared Value is lower for test data:
+- So looks like in -sample, out-of-sample is not as robust as we might want,
+- since our R-squared value is less using test data than when using train data
