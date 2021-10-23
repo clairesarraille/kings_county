e423229c7ba92fe8a19d1800a91d9f36456f7557
Claire Sarraillé <clairesarraille@outlook.com>
2021-10-22 22:34:29 -0700

    Narrow to draft for review

--- dsc-phase-2-project-main/drafts/000_dfs.py
+++
@@ -1,3 +0,0 @@
-R-Squared Value is lower for test data:
-- So looks like in -sample, out-of-sample is not as robust as we might want,
-- since our R-squared value is less using test data than when using train data
--- dsc-phase-2-project-main/drafts/notes/tmp.py
+++
@@ -1,2 +0,0 @@
-waterfront_coef_lis = pd.Series([0.0094, 0.0601, 0.4775, -0.0506, -0.0117, 0.0779, 0.058, 0.104, -0.0737], index=[
-                                'bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'condition', 'grade', 'yr_built'])
